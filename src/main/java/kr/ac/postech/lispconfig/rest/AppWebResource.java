21887e8fbcdb647b8fe43b1cf4cd55f805a670e8
yoonseon <yoonseon@onlab.us>
2016-11-08 11:14:07 -0800

    LOCAL-DB rest APIs are added.

--- src/main/java/kr/ac/postech/lispconfig/rest/AppWebResource.java
+++ src/main/java/kr/ac/postech/lispconfig/rest/AppWebResource.java
@@ -18,12 +18,18 @@ package kr.ac.postech.lispconfig.rest;
 
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableList;
 import kr.ac.postech.lispconfig.LispConfigService;
+import org.onlab.packet.IpAddress;
+import org.onosproject.lisp.msg.protocols.DefaultLispLocatorRecord;
+import org.onosproject.lisp.msg.protocols.DefaultLispMapRecord;
+import org.onosproject.lisp.msg.types.LispIpv4Address;
 import org.onosproject.net.DeviceId;
 import org.onosproject.net.device.DeviceService;
 import org.onosproject.rest.AbstractWebResource;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -32,6 +38,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.InputStream;
 
 
 /**
@@ -41,6 +48,7 @@ import javax.ws.rs.core.Response;
 public class AppWebResource extends AbstractWebResource {
 
     static final String ITR_CFG = "<itr-cfg/>";
+    static final String ETR_CFG = "<etr-cfg/>";
 
     /**
      * Get hello world greeting.
@@ -59,37 +67,13 @@ public class AppWebResource extends AbstractWebResource {
         return ok(node).build();
     }
 
-    @GET
-    @Path("/map-resolver/{deviceId}")
-    @Produces(MediaType.APPLICATION_XML)
-    public Response getMapResolvers(@PathParam("deviceId") String deviceId) {
-
-        LispConfigService service = get(LispConfigService.class);
-        DeviceId devId = DeviceId.deviceId(deviceId);
-        String result = service.getConfigWithFilter(devId, ITR_CFG);
-
-        return ok(result).build();
-    }
-
-    @POST
-    @Path("/map-resolver/{deviceId}")
-    public Response addMapResolvers(@PathParam("deviceId") String deviceId,
-    @QueryParam("address") String address) {
-
-        LispConfigService service = get(LispConfigService.class);
-        DeviceId devId = DeviceId.deviceId(deviceId);
-        boolean result = service.addItrMapResolver(devId, address);
-
-        return ok(result).build();
-    }
-
     @POST
     @Path("/devices")
     public Response connect(@QueryParam("username") String username,
                             @QueryParam("password") String password,
                             @QueryParam("address") String address,
                             @QueryParam("port") String port
-                            ) {
+    ) {
 
         LispConfigService service = get(LispConfigService.class);
         boolean result = service.connectDevice(username, password, address, port);
@@ -110,6 +94,129 @@ public class AppWebResource extends AbstractWebResource {
         );
 
         return ok(node).build();
+    }
+
+    @GET
+    @Path("{deviceId}/map-resolver")
+    @Produces(MediaType.APPLICATION_XML)
+    public Response getMapResolvers(@PathParam("deviceId") String deviceId) {
+
+        LispConfigService service = get(LispConfigService.class);
+        DeviceId devId = DeviceId.deviceId(deviceId);
+        String result = service.getConfigWithFilter(devId, ITR_CFG);
+
+        return ok(result).build();
+    }
+
+    @POST
+    @Path("{deviceId}/map-resolver")
+    public Response addMapResolvers(@PathParam("deviceId") String deviceId,
+    @QueryParam("address") String address) {
+
+        LispConfigService service = get(LispConfigService.class);
+        DeviceId devId = DeviceId.deviceId(deviceId);
+        boolean result = service.addItrMapResolver(devId, address);
+
+        return ok(result).build();
+    }
+
+    @DELETE
+    @Path("{deviceId}/map-resolver")
+    public Response removeMapResolvers(@PathParam("deviceId") String deviceId,
+                                    @QueryParam("address") String address) {
+
+        LispConfigService service = get(LispConfigService.class);
+        DeviceId devId = DeviceId.deviceId(deviceId);
+        boolean result = service.removeItrMapResolver(devId, address);
+
+        return ok(result).build();
+    }
+
+    @GET
+    @Path("{deviceId}/local-db")
+    @Produces(MediaType.APPLICATION_XML)
+    public Response getLocalDb(@PathParam("deviceId") String deviceId) {
+        LispConfigService service = get(LispConfigService.class);
+        DeviceId devId = DeviceId.deviceId(deviceId);
+        String result = service.getConfigWithFilter(devId, ETR_CFG);
+
+        return ok(result).build();
+    }
+
+    @POST
+    @Path("{deviceId}/local-db")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addLocalDb(@PathParam("deviceId") String deviceId,
+                               @QueryParam("eid") String eid,
+                               @QueryParam("eid_mask") Byte eid_mask,
+                               @QueryParam("rloc") String rloc,
+                               @QueryParam("priority") byte priority,
+                               @QueryParam("weight") byte weight,
+                               InputStream inputStream) {
+        if(inputStream == null) {
+            LispConfigService service = get(LispConfigService.class);
+            DeviceId devId = DeviceId.deviceId(deviceId);
+
+            DefaultLispMapRecord.DefaultMapRecordBuilder builder =
+                    new DefaultLispMapRecord.DefaultMapRecordBuilder();
+
+            builder.withEidPrefixAfi(new LispIpv4Address(IpAddress.valueOf(eid)));
+            builder.withMaskLength(eid_mask);
+
+            DefaultLispLocatorRecord.DefaultLocatorRecordBuilder locatorRecordBuilder
+                    = new DefaultLispLocatorRecord.DefaultLocatorRecordBuilder();
+
+            locatorRecordBuilder.withLocatorAfi(new LispIpv4Address(IpAddress.valueOf(rloc)));
+            locatorRecordBuilder.withPriority(priority);
+            locatorRecordBuilder.withWeight(weight);
+
+            builder.withLocators(ImmutableList.of(locatorRecordBuilder.build()));
+
+            Boolean result = service.addEtrEidDataBase(devId, builder.build());
+
+            return ok(result).build();
+        } else {
+            //TODO: JSON support
+        }
+        return ok(false).build();
+    }
+
+    @DELETE
+    @Path("{deviceId}/local-db")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response removeLocalDb(@PathParam("deviceId") String deviceId,
+                               @QueryParam("eid") String eid,
+                               @QueryParam("eid_mask") Byte eid_mask,
+                               @QueryParam("rloc") String rloc,
+                               @QueryParam("priority") byte priority,
+                               @QueryParam("weight") byte weight,
+                               InputStream inputStream) {
+        if(inputStream == null) {
+            LispConfigService service = get(LispConfigService.class);
+            DeviceId devId = DeviceId.deviceId(deviceId);
+
+            DefaultLispMapRecord.DefaultMapRecordBuilder builder =
+                    new DefaultLispMapRecord.DefaultMapRecordBuilder();
+
+            builder.withEidPrefixAfi(new LispIpv4Address(IpAddress.valueOf(eid)));
+            builder.withMaskLength(eid_mask);
+
+            DefaultLispLocatorRecord.DefaultLocatorRecordBuilder locatorRecordBuilder
+                    = new DefaultLispLocatorRecord.DefaultLocatorRecordBuilder();
+
+            locatorRecordBuilder.withLocatorAfi(new LispIpv4Address(IpAddress.valueOf(rloc)));
+            locatorRecordBuilder.withPriority(priority);
+            locatorRecordBuilder.withWeight(weight);
+
+            builder.withLocators(ImmutableList.of(locatorRecordBuilder.build()));
+
+            Boolean result = service.removeEtrEidDataBase(devId, builder.build());
+
+            return ok(result).build();
+        } else {
+            //TODO: JSON support
+        }
+        return ok(false).build();
     }
 
 }
